128a17f5829bf5b804359d2853b564d9dc4b311b
Nick Ridge <110374048+Reginald9512@users.noreply.github.com>
2022-10-28 10:06:03 +1100

    In-Game UI & Pause Menu

---
+++ GAD181 - Project 2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public static bool GameIsPaused = false;
+
+    public GameObject controlsPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        GameIsPaused = true;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        GetComponent<PlayerController>().enabled = false;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        GameIsPaused = false;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        GetComponent<PlayerController>().enabled = true;
+    }
+
+    public void Instructions(int sceneID)
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void OpenControls()
+    {
+        controlsPanel.SetActive(true);
+    }
+}
